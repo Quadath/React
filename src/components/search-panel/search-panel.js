22dbbb2fc55e16643acb21ff7a03aacc8df291d7
Quadatz <quadath@gmail.com>
2022-02-22 09:42:21 +0200

    interactive 2

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchPanel = () => {
-    return (
-        <input className='form-control search-input'
-        type="text"
-        placeholder='Поиск по записям'></input>
-    )
+export default class SearchPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        }
+        this.onValueChange = this.onValueChange.bind(this);
+    }
+    onValueChange(e) {
+        const term = e.target.value;
+        this.setState({term});
+        this.props.onUpdateSearch(term);
+    }
+    render() {
+        return (
+            <input className='form-control search-input'
+            type="text"
+            placeholder='Поиск по записям'
+            onChange={this.onValueChange}></input>
+        )
+    }
 }
-export default SearchPanel;
